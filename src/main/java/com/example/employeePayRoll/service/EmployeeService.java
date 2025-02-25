403d7181bfddae81f1fb0eb0f48b222b0f8d28b3
shrey <shreya272002gupta@gmail.com>
2025-02-25 10:32:06 +0530

    [Shreya] Add. Employee Service Program

---
+++ src/main/java/com/example/employeePayRoll/service/EmployeeService.java
@@ -0,0 +1,46 @@
+package com.example.employeePayRoll.service;
+
+import com.example.employeePayRoll.exception.ResourceNotFoundException;
+import com.example.employeePayRoll.model.Employee;
+import com.example.employeePayRoll.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with ID: " + id));
+    }
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Long id, Employee updatedEmployee) {
+        Employee existingEmployee = getEmployeeById(id);
+        existingEmployee.setName(updatedEmployee.getName());
+        existingEmployee.setSalary(updatedEmployee.getSalary());
+        existingEmployee.setDepartment(updatedEmployee.getDepartment());
+        return employeeRepository.save(existingEmployee);
+    }
+
+    public void deleteEmployee(Long id) {
+        Employee existingEmployee = getEmployeeById(id);
+        employeeRepository.delete(existingEmployee);
+    }
+}
