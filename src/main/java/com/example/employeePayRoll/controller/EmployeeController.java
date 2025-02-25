0968c64c3230d25f691fffb7d770a04e8440a326
shrey <shreya272002gupta@gmail.com>
2025-02-25 10:20:00 +0530

    [Shreya] Add. Employee Controller Program

---
+++ src/main/java/com/example/employeePayRoll/controller/EmployeeController.java
@@ -0,0 +1,46 @@
+package com.example.employeePayRoll.controller;
+
+import com.example.employeePayRoll.model.Employee;
+import com.example.employeePayRoll.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+@CrossOrigin(origins = "http://localhost:3000") // Allow frontend access
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable Long id) {
+        return employeeService.getEmployeeById(id);
+    }
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+
+    @PutMapping("/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(id, employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEmployee(@PathVariable Long id) {
+        employeeService.deleteEmployee(id);
+    }
+}
