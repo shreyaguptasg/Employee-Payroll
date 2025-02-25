f69a217476e137c5e33df8c7a20abbdf632d7124
shrey <shreya272002gupta@gmail.com>
2025-02-25 09:53:37 +0530

    [Shreya] Add. Setup for Employee Payroll

---
+++ src/test/java/com/example/employeePayRoll/EmployeePayRollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeePayRoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayRollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
