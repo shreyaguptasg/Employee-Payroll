b1b9d77270bdb780a40ab4defd0758543e19372f
shrey <shreya272002gupta@gmail.com>
2025-02-28 16:37:07 +0530

    Storing data in service layer

--- src/main/java/com/example/employeePayRoll/model/Employee.java
+++ src/main/java/com/example/employeePayRoll/model/Employee.java
@@ -14,46 +14,11 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Getter @Setter
     private String name;
     private String department;
     private double salary;
 
-//    public Employee(Long id, String name, String department, double salary) {
-//        this.id = id;
-//        this.name = name;
-//        this.department = department;
-//        this.salary = salary;
-//    }
 
-    public Long getId() {
-        return id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
